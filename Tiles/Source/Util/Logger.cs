1d81757a8c63586c2055d351f3169dc0ca6a33a8
DrSmCraft <samuil2000@hotmail.com>
2018-11-26 00:18:42 -0500

    Test Commit

---
+++ Tiles/Source/Util/Logger.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGGame.Source.Util
+{
+	public enum LogType
+	{
+		INFO, WARNING, ERROR
+	}
+
+	public class Logger
+	{
+		private static List<string> array = new List<string>();
+
+		public static void Log(LogType type, string msg)
+		{
+			string st = "[" + DateTime.Now + "] " + "[" + type + "] " + msg;
+			st = st.Replace("\n", "\n\t\t");
+			array.Add(st);
+		}
+
+		public static string ToString()
+		{
+			var stringBuilder = new StringBuilder();
+			stringBuilder.AppendLine("-------------LOG-------------");
+			stringBuilder.AppendLine("Num:\t" + "Time:\t" + "Message...........");
+
+			for (var i = 0; i < array.Count; i++)
+				stringBuilder.AppendLine(array[i]);
+
+			return stringBuilder.ToString();
+		}
+
+		public static void Print()
+		{
+			Console.Out.Write(ToString());
+		}
+
+		public static void WriteToFile(string filename)
+		{
+			if (!File.Exists(filename))
+				using (var fs = File.Create(filename))
+				{
+					fs.Flush();
+				}
+
+			using (var writer = new StreamWriter(filename))
+			{
+				writer.Flush();
+				writer.Write(ToString());
+			}
+		}
+	}
+}
+
